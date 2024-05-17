0703938ba0f921eed5f9983be6ffe7aea70954e3
yugandharjonnadhula <163026370+yugandharjonnadhula@users.noreply.github.com>
2024-05-17 18:05:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function firstWord(s) {
   // your code here
+	const trimmedstr=str.trim();
+	const words=trimmedstr.split(" ");
+	return words.length > 0 ? words[0] : trimmedstr;
 }
 
 // Do not change the code below
